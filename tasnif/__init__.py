eff57992f07caeebb1b91b664bb34dead926d42d
Mert Cobanov <mertcobanov@gmail.com>
2024-03-11 14:09:37 +0300

    Refactor imports in tasnif/__init__.py

--- tasnif/__init__.py
+++ tasnif/__init__.py
@@ -2,7 +2,6 @@
 Tasnif is a simple library for image classification and clustering.
 """
 
-__version__ = "0.1.0"
 __author__ = "Mert Cobanov"
 __author_email__ = "mertcobanov@gmail.com"
 __license__ = "MIT"
@@ -11,5 +10,9 @@ __url__ = "https://github.com/cobanov/tasnif"
 
 from tasnif.calculations import calculate_kmeans, calculate_pca, get_embeddings
 from tasnif.tasnif import Tasnif
-from tasnif.utils import (create_dir, create_image_grid,
-                          read_images_from_directory, read_with_pil)
+from tasnif.utils import (
+    create_dir,
+    create_image_grid,
+    read_images_from_directory,
+    read_with_pil,
+)
